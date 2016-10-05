df650ecf34771757858b58479e1bc1802b9dddd3
Dylan <Dylan@Dylan>
2016-10-05 08:21:03 +0200

    ProductDatabaseSQL.2

--- src/database/ProductDatabaseSQL.java
+++ src/database/ProductDatabaseSQL.java
@@ -113,55 +113,55 @@ public class ProductDatabaseSQL {
 	}
 	
 	
-=======
-import java.util.List;
-
-import domain.Product;
-import exception.DomainException;
-
-public class ProductDatabaseSQL implements IProductDatabase {
-
-	@Override
-	public Product getProduct(int id) throws DomainException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Product> getAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addProduct(Product p) throws DomainException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateProduct(Product p) throws DomainException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteProduct(int id) throws DomainException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void open() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
-		
-	}
+//=======
+//import java.util.List;
+//
+//import domain.Product;
+//import exception.DomainException;
+//
+//public class ProductDatabaseSQL implements IProductDatabase {
+//
+//	@Override
+//	public Product getProduct(int id) throws DomainException {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<Product> getAllProducts() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public void addProduct(Product p) throws DomainException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void updateProduct(Product p) throws DomainException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteProduct(int id) throws DomainException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void open() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void close() {
+//		// TODO Auto-generated method stub
+//		
+//	}
 >>>>>>> branch 'master' of https://github.com/MathiasSpanhove/groep_project_1617.git
 
 }
