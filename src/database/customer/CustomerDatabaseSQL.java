d931002dcc3cc4c9dfd404a79db31bf3e85b76b0
Dylan <Dylan@Dylan>
2016-10-16 02:03:31 +0200

    getAllCustomers, addCustomer, deleteCustomer, open & close

--- src/database/customer/CustomerDatabaseSQL.java
+++ src/database/customer/CustomerDatabaseSQL.java
@@ -1,12 +1,19 @@
 package database.customer;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import domain.Shop;
 import domain.customer.Customer;
+import domain.product.Product;
+import domain.product.enums.Products;
 import exception.DatabaseException;
 
 public class CustomerDatabaseSQL implements ICustomerDatabase {
@@ -39,20 +46,59 @@ public class CustomerDatabaseSQL implements ICustomerDatabase {
 	
 	@Override
 	public Customer getCustomer(int id) throws DatabaseException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Customer> getAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Customer> getAllCustomers() throws DatabaseException {
+		List<Customer> customers = new ArrayList<>();
+		String sql = "SELECT * FROM customer";
+		
+		try{
+			statement = connection.prepareStatement(sql);
+			ResultSet result = statement.executeQuery();
+			
+			while(result.next()){
+				String firstName = result.getString("firstname");
+				String lastName = result.getString("lastname");
+				String email = result.getString("email");
+				int id = Integer.parseInt(result.getString("id"));
+				Boolean subscribed = result.getBoolean("subscribed");
+				
+				Customer c = new Customer(firstName, lastName, email, id, subscribed, shop);
+				customers.add(c);
+			}
+			return customers;
+		}catch(Exception e){
+			throw new DatabaseException(e.getMessage());
+		}finally{
+			this.close();
+		}
 	}
+
+
 
 	@Override
 	public void addCustomer(Customer c) throws DatabaseException {
-		// TODO Auto-generated method stub
-		
+		if(c == null){
+			throw new DatabaseException("Nothing to add");
+		}
+		else{
+		this.open();
+		String sql = "INSERT INTO Customer (firstname, lastname, email)" + " VALUES(?,?,?)";
+		try{
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, c.getFirstName());
+			statement.setString(2, c.getLastName());
+			statement.setString(3, c.getEmail());
+			statement.execute();
+			System.out.println("Great succes");
+		}catch(Exception e){
+			throw new DatabaseException(e.getMessage());
+		}finally{
+			this.close();
+		}
+		}
 	}
 
 	@Override
@@ -63,8 +109,17 @@ public class CustomerDatabaseSQL implements ICustomerDatabase {
 
 	@Override
 	public void deleteCustomer(int id) throws DatabaseException {
-		// TODO Auto-generated method stub
+		this.open();
 		
+		String sq1 = "DELETE FROM customer " + "WHERE id = '" + id + "'";
+		try {
+			this.statement = this.connection.prepareStatement(sq1);
+			this.statement.execute();
+		} catch (Exception e) {
+			throw new DatabaseException(e.getMessage());
+		} finally {
+			this.close();
+		}
 	}
 	
 	@Override
@@ -75,14 +130,25 @@ public class CustomerDatabaseSQL implements ICustomerDatabase {
 	
 	@Override
 	public void open() {
-		// TODO Auto-generated method stub
-		
+		try {
+			this.connection = DriverManager.getConnection(URL, this.properties);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-		
+		try {
+			if (statement != null) {
+		    	statement.close();
+			}
+			if (connection != null) {
+		    	connection.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/database/customer/ICustomerDatabase.java
+++ src/database/customer/ICustomerDatabase.java
@@ -7,7 +7,7 @@ import exception.DatabaseException;
 public interface ICustomerDatabase {
 
 	public Customer getCustomer(int id) throws DatabaseException;
-	public List<Customer> getAllCustomers();
+	public List<Customer> getAllCustomers() throws DatabaseException;
 	public void addCustomer(Customer c) throws DatabaseException;
 	public void updateCustomer(Customer c) throws DatabaseException;
 	public void deleteCustomer(int id) throws DatabaseException;
